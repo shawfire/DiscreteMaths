3f19545b695fa280da09a0499581ebfde17b38ee
John Shaw <shaw_fire@yahoo.com>
2017-05-16 14:05:36 +1000

    PhaseA rules and more test.

--- PhaseA/spec/phasea_type_spec.rb
+++ PhaseA/spec/phasea_type_spec.rb
@@ -26,12 +26,31 @@ describe 'PhaseAType' do
     it "returns the phase type 2" do
       expect(phasea_phase_type([['2C', '2C', '4C', 'KC', '9C', 'AH', 'JC']])).to eq(2)
       expect(phasea_phase_type([['2H', '2H', '4H', 'KH', '9H', 'AD', 'JH']])).to eq(2)
-
+      expect(phasea_phase_type([['2H', '2H', '4H', 'KH', '9H', 'KH', 'JH']])).to eq(2)
     end
 
     it "returns nil if invalid phase 2" do
       expect(phasea_phase_type([['2C', '2C', '4C', 'KC', '9C', 'AH', 'JH']])).to eq(nil)
     end
+
+    it "returns the phase type 3" do
+      expect(phasea_phase_type([['4H', '4S', 'AC', '4C']])).to eq(3)
+      expect(phasea_phase_type([['4H', '4S', '4C', '4C']])).to eq(3)
+    end
+
+    it "returns nil if invalid phase 3" do
+      expect(phasea_phase_type([['4H', '4S', 'KC', '4C']])).to eq(nil)
+    end
+
+    it "returns the phase type 4" do
+      expect(phasea_phase_type([['4H', '5S', 'AC', '7C', '8H', 'AH', '0S', 'JC']])).to eq(4)
+      expect(phasea_phase_type([['AH', '5S', 'AC', '7C', '8H', 'AH', '0S', 'JC']])).to eq(4)
+    end
+
+    it "returns nil if invalid phase 4" do
+      expect(phasea_phase_type([['AH', '5S', 'AC', '7C', '8H', 'AH', '0S', 'QC']])).to eq(nil)
+    end
+
 
 
   end
